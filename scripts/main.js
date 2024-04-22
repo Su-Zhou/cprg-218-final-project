443b69c56713dcde0ab02d489d0f1ee3dc4b73ac
Susane <147738127+Su-Zhou@users.noreply.github.com>
2024-04-22 17:35:16 -0600

    docs: restored files

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+console.log('Thank you for teaching me these two semesters! I have learned a lot that I never thought I would in terms of coding');
+
+//dynamic date
+document.addEventListener("DOMContentLoaded", () => {
+    const date = new Date().getFullYear();
+    document.getElementById("year").innerHTML = date;
+  });
+
+
+//navbar hamburger menu responsiveness
+  const myFunction = () => {
+    const x = document.getElementById("myNavbar");
+    if (!x.classList.contains("responsive")) {
+        x.classList.add("responsive"); 
+    } else {
+        x.classList.remove("responsive"); 
+    }
+};
+
+const navigateToShop = () => {
+  window.location.href = 'shop/index.html';
+};
